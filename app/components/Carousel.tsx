f3af359555fe4f290f09adc30e9b7517362ff8f4
David Doria <daviddoria@Davids-M1-MacBook-Pro.local>
2024-11-12 14:04:37 -0800

    got resize from bottom to work

--- app/components/Carousel.tsx
+++ app/components/Carousel.tsx
@@ -11,19 +11,16 @@ const Carousel = ({
   active: number;
 }) => {
   return (
-    <div className="lg:w-[75%] mt-20 text-white grid grid-cols-3 md:flex gap-2 md:gap-4 z-20 absolute bottom-0 p-12 sm:p-20">
-      {/* <div className="flex flex-wrap gap-4 lg:w-[75%] mt-20 text-white"> */}
+    <div className="lg:w-[75%] mt-20 text-white z-20 absolute bottom-0 p-12 sm:p-20 grid grid-cols-3 md:flex gap-2 md:gap-4 md:items-end">
       {images.map((pic, idx) => (
         <div
           key={idx}
           className={clsx(
             "rounded-2xl cursor-pointer md:w-[20%] hover:scale-110 duration-200 p-2",
-            // idx === 2 ? "max-w-[150%] grow-1" : "grow-0"
-            active === idx ? "border-[4px] " : ""
+            active === idx ? "border-[4px] active basis-1/3" : ""
           )}
           style={{
             borderImage: "linear-gradient(135deg, black, white, black) 1",
-            // "linear-gradient(135deg, black, white) 1",
           }}
         >
           <img
